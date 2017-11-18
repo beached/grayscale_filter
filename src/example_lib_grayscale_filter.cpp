4faf517b75df94470798068075d40b9112781427
Darrell Wright <dwright-git@dawdevel.ca>
2017-11-18 14:40:47 -0500

    Rearranged project

--- src/example_lib_grayscale_filter.cpp
+++ src/example_lib_grayscale_filter.cpp
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 Darrell Wright
+// Copyright (c) 2016-2017 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files( the "Software" ), to deal
@@ -20,25 +20,22 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <cstdlib>
 #include <cassert>
-#include <daw/grayscale_filter/genericimage.h>
-#include <daw/grayscale_filter/filterdawgs2.h>
 #include <chrono>
+#include <cstdlib>
 
-int main( int argc, char ** argv ) {
-	assert( argc > 2 );
+#include <daw/daw_benchmark.h>
+#include <daw/daw_exception.h>
+#include <daw/grayscale_filter/filterdawgs2.h>
+#include <daw/grayscale_filter/genericimage.h>
+
+int main( int argc, char **argv ) {
+	daw::exception::daw_throw_on_false( argc >= 3, "Must supply a source and destination file" );
 	using namespace daw::imaging;
 
-	auto start = std::chrono::system_clock::now( );
-	FilterDAWGS2::filter( from_file( argv[1] ) ).to_file( argv[2] );
-	auto end = std::chrono::system_clock::now( );
+	auto const elapsed = daw::benchmark( [argv]( ) { FilterDAWGS2::filter( from_file( argv[1] ) ).to_file( argv[2] ); } );
 
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	auto end_time = std::chrono::system_clock::to_time_t( end );
-
-	std::cout << "finished computation at " << std::ctime( &end_time ) << "elapsed time: " << elapsed_seconds.count( ) << "s\n";
-
+	std::cout << "elapsed time: " << daw::utility::format_seconds( elapsed, 2 ) << '\n';
 
 	return EXIT_SUCCESS;
 }
